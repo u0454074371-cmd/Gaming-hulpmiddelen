abda059db30dd9b1211be2402c6ac76d41ec654b
u0454074371-cmd <u0454074371@gmail.com>
2026-08-14 21:52:48 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'orbit-timer-v1';
+const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
